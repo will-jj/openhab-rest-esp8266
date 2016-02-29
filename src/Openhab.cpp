8dd6e767d0fc0b81f68bb65f57fadc95873b6b7e
Brenda Wallace <brenda@brenda.nz>
2016-02-29 21:02:07 +1300

    First draft of library
    get and post working over wifi

---
+++ src/Openhab.cpp
@@ -0,0 +1,70 @@
+/**
+ * Openhab.cpp
+ *
+ */
+
+#include "Arduino.h"
+#include "ESP8266WiFi.h"
+#include "Openhab.h"
+
+#ifdef ARDUINO_ARCH_AVR
+#include <avr/pgmspace.h>
+#else
+#include "pgmspace.h"
+#endif
+
+Openhab::Openhab(const char* host, const int port) {
+  _host = host;
+  _port = port;
+  _params = "";
+}
+
+String Openhab::url(String item) {
+
+  String result = "http://" + (String)_host + ":" + (String)_port + "/rest/items/" + item + "/state";
+  _params = "";
+  return result;
+
+}
+
+String Openhab::get(String item) {
+  String url = Openhab::url(item);
+  String request = "GET " + url + " HTTP/1.1\n";
+  request += "Host: " + (String)_host + "\n";
+  request += "Connection: close\n";
+
+  return Openhab::send(request);
+ }
+
+String Openhab::put(String item, String state) {
+  String url = Openhab::url(item);
+  
+  String request = "PUT " + url + " HTTP/1.1\n";
+  request += "Host: " + (String)_host + "\n";
+
+  int state_length = state.length();
+  request += "Content-Length: " + (String) state_length + "\n";
+
+  request += "Connection: close\n";
+  request += "\n";
+
+  request += state;
+                   
+  return Openhab::send(request);
+}
+
+String Openhab::send(String request) {
+  WiFiClient client;
+  if (!client.connect(_host, _port)) {
+    return "ERROR: failed to connect";
+  }
+
+  client.print(request);
+  delay(10);
+
+  String response;  
+  while(client.available()){
+    response += client.readStringUntil('\r');
+  }
+  return response;
+}
---
+++ src/Openhab.h
@@ -0,0 +1,32 @@
+/**
+ * Openhab.h
+ *
+ */
+
+#ifndef Openhab_h
+#define Openhab_h
+
+#include "Arduino.h"
+#include "ESP8266WiFi.h"
+
+
+class Openhab {
+
+  public:
+    Openhab(const char* host, const int port);
+
+    String url(String item);
+
+    String get(String item);
+    String put(String item, String state);
+
+    String send(String request);
+
+  private:
+    const char* _host;
+    int    _port;
+    String _params;
+ 
+};
+
+#endif
